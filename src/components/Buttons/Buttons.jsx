732114c697837e79986d7414b49ff3b9c1f65ba1
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-02-26 13:14:15 +0200

    changed to hooks using

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const capitalize = s => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+
+const Buttons =  ({onClick, options})  =>{
+    return (
+      <Wrapper>
+        {options.map(option => (
+          <FeedbackBtnsItem key={option}>
+            <button onClick={() => onClick(option)} value={option}>
+              {capitalize(option)}
+            </button>
+          </FeedbackBtnsItem>
+        ))}
+      </Wrapper>
+    );
+  }
+
+
+
+const Wrapper = styled.ul`
+  display: flex;
+`;
+
+const FeedbackBtnsItem = styled.li`
+  margin-right: 10px;
+`;
+
+Buttons.propTypes = {
+  onClick: PropTypes.func,
+};
+
+export default Buttons;
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,58 @@
+import React, { useState  } from 'react';
+import styled from 'styled-components';
+
+import Buttons from '../Buttons/Buttons';
+import Stats from '../Stats/Stats';
+
+
+const Feedback = ()=> {
+  const [feedback, setFeedback] = useState ({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  
+
+  const onClickAddToStat = value => {
+    setFeedback (prevState => {
+      return { ...prevState, [value]: prevState[value] + 1 };
+    });
+  };
+
+ const  countTotalFeedback = () => {
+    return Object.values(feedback).reduce(
+      (prevValue, currValue) => prevValue + currValue,
+      0
+    );
+  };
+
+  const countPositiveFeedbackPercentage = (total) => {
+    return total > 0 ? +((100 / total) * feedback.good).toFixed(): 0;
+  };
+
+  
+    return (
+      <Container>
+        <h2>Please leave feedback</h2>
+        <Buttons
+          onClick={onClickAddToStat}
+          options={Object.keys(feedback)}
+        />
+        <h2>Statistics</h2>
+        <Stats
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={countTotalFeedback()}
+          positivePercent={countPositiveFeedbackPercentage()}
+        />
+      </Container>
+    );
+  }
+
+
+const Container = styled.div`
+  margin: 0 15px;
+`;
+
+export default Feedback;
